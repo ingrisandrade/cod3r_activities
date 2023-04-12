16ee7d97cf2b49b5e150d0d7aba96d138c254782
Ingris Andrade <iac22@outlook.com>
2023-04-12 11:20:21 -0300

    Reviewing filter

--- map_reduce_filter.js
+++ map_reduce_filter.js
@@ -90,7 +90,36 @@ const aprovado = (nota) => nota >= 7;
 const resultadoNotas = notas.filter(aprovado);
 console.log(resultadoNotas);
 
+const carrinho2 = [
+  { nome: 'Caneta', qtde: 10, preco: 7.99 },
+  { nome: 'Impressora', qtde: 0, preco: 649.50 },
+  { nome: 'Caderno', qtde: 4, preco: 27.10 },
+  { nome: 'Lapis', qtde: 3, preco: 5.82 },
+  { nome: 'Tesoura', qtde: 0, preco: 19.20 },
+];
+
+const qtdeMaiorqueZero = item => item.qtde > 0;
+const itensValidos = carrinho2.filter(qtdeMaiorqueZero);
+console.log(itensValidos);
+
 // map e filter aplicados juntos, o resultado do map depende do resultado do filter.
+
+const carrinho3 = [
+  { nome: 'Caneta', qtde: 10, preco: 7.99 },
+  { nome: 'Impressora', qtde: 0, preco: 649.50 },
+  { nome: 'Caderno', qtde: 4, preco: 27.10 },
+  { nome: 'Lapis', qtde: 3, preco: 5.82 },
+  { nome: 'Tesoura', qtde: 0, preco: 19.20 },
+];
+
+const getNome3 = item => item.nome;
+const qtdeMaiorqueZero2 = item => item.qtde > 0;
+const nomeItensValidos = carrinho3
+  .filter(qtdeMaiorqueZero2)
+  .map(getNome3)
+console.log(nomeItensValidos);
+
+// -
 
 const produtos   = [
   { nome: '...', preco: 23.67, desconto: 0.2 },
