1c9eec6b51d1c953d45719bebf63ad7b379580b4
Ingris Andrade <iac22@outlook.com>
2023-04-13 11:44:06 -0300

    Reviewing reduce

--- map_reduce_filter.js
+++ map_reduce_filter.js
@@ -203,3 +203,22 @@ const valorInicial = {total: 0, qtde: 0, media: 0};
 const valorFinal = numerosReduce3.reduce(media, valorInicial);
 
 console.log(valorFinal.media);
+
+// -
+
+const carrinhoReduce = [
+  { nome: 'Caneta', qtde: 10, preco: 7.99 },
+  { nome: 'Impressora', qtde: 0, preco: 649.50 },
+  { nome: 'Caderno', qtde: 4, preco: 27.10 },
+  { nome: 'Lapis', qtde: 3, preco: 5.82 },
+  { nome: 'Tesoura', qtde: 1, preco: 19.20 },
+];
+
+const getTotalReduce = item => item.qtde * item.preco;
+const somarReduce = (acc, el) => acc + el;
+
+const totalGeral = carrinhoReduce
+  .map(getTotalReduce)
+  .reduce(somarReduce)
+
+console.log((totalGeral));
