f8511444253d705ac7d3753656008e8fd3071c04
Ingris Andrade <iac22@outlook.com>
2023-05-05 17:05:13 -0300

    :sparkles: Refactor challenge.

--- database/first-steps/select-planet-2.sql
+++ database/first-steps/select-planet-2.sql
@@ -1,17 +1,31 @@
-SELECT nome, raio FROM planeta;
+SELECT nome, raio * 2 FROM planeta;
+
 -- seleciona planeta -> nome, diametro.
 
-SELECT * FROM planeta WHERE nome <> 'Tatooine';
+SELECT id, nome FROM planeta WHERE nome <> 'Tatooine';
+
 -- seleciona planeta -> nome <> 'Tatooine'.
 
-SELECT * FROM planeta WHERE habitado = 1;
+SELECT id, nome, habitado FROM planeta WHERE habitado = 1;
+
 -- seleciona planeta habitado.
 
-SELECT * FROM planeta WHERE habitado = 0;
+SELECT id, nome, habitado FROM planeta WHERE habitado = 0;
+
 -- seleciona planeta não habitado.
 
-SELECT * FROM planeta WHERE id % 2 = 0;
+SELECT id, nome, habitado FROM planeta WHERE !habitado;
+
+-- segunda forma de selacionar não habitado.
+
+SELECT id, nome, habitado FROM planeta WHERE NOT habitado;
+
+-- terceira forma de selacionar não habitado.
+
+SELECT id, nome, habitado FROM planeta WHERE id % 2 = 0;
+
 -- seleciona planeta -> id par.
 
 SELECT * FROM planeta WHERE id % 2 <> 0;
+
 -- seleciona planeta -> id ímpar.
