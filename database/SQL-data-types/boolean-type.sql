a9bb84ee46b2bef3f6dd293f0650c4a47c2e1bb0
Ingris Andrade <iac22@outlook.com>
2023-05-06 16:23:21 -0300

    :sparkles: Add boolean type.

---
+++ database/SQL-data-types/boolean-type.sql
@@ -0,0 +1,37 @@
+DROP TABLE aluno;
+
+CREATE TABLE
+    aluno (
+        matricula CHAR(10) NOT NULL,
+        -- o char ocupa todos o espaço reservado no comando para armazenar o valor.
+        nome VARCHAR(120) NOT NULL,
+        -- o varchar ocupa um espaço menor no disco, mas ele coloca um espaço adicional.
+        nome_social VARCHAR(80),
+        semestre TINYINT,
+        creditos_cursados SMALLINT,
+        media_geral DECIMAL(4, 2),
+        -- Se for ponto flutuante deve-se usar DOUBLE.
+        data_ingresso DATE NOT NULL,
+        -- Caso queira considerar data e hora, usa-se DATETIME, e no INSERT coloca data e hora | 2021-02-10 13:44:13|.
+        data_conclusao DATE,
+        sexo ENUM('masculino', 'feminino'),
+        areas_interesse SET('Big Data', 'Banco de Dados', 'Desenvolvimento'),
+        bolsista BOOLEAN
+    );
+
+INSERT INTO aluno
+VALUES (
+        "202106A6F7",
+        "Marlos Pinheiro Borges",
+        "Marlin",
+        3,
+        56,
+        8.7,
+        "2021-02-10",
+        NULL,
+        "masculino",
+        "Banco de Dados,Desenvolvimento",
+        true
+    );
+
+SELECT * FROM aluno;
